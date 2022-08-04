07e616b7a366e3a1b9e141b29a51fcc43d4e64d4
Fasica Zewdie <58349651+Fasica37@users.noreply.github.com>
2022-08-04 11:01:39 +0300

    initail commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_payment_with_stripe/pages/home_page.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+import '.env';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = stripePublishableKey;
+  await Stripe.instance.applySettings();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
